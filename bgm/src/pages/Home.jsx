ec483004f0a997accec5f6fdca1eaaf4c375e4bd
Srikanth Amudala <srikanthamudala914@gmail.com>
2025-08-22 20:20:27 +0530

    Initial commit with frontend and server

---
+++ bgm/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header'
+import Ste from '../components/Ste'
+import Bgslider from '../components/Bgslider'
+import Testimonials from '../components/Testimonials'
+import Upload from '../components/Upload'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Ste />
+      <Bgslider />
+      <Testimonials />
+      <Upload />
+    </div>
+  )
+}
+
+export default Home
